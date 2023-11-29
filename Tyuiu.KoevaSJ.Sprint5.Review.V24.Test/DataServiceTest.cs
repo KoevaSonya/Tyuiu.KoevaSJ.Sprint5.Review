6e62328e01d37a6aa118aeaf1bd5d5b7b6307285
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-11-29 10:26:49 +0500

    Add project files.

---
+++ Tyuiu.KoevaSJ.Sprint5.Review.V24.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.KoevaSJ.Sprint5.Review.V24.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint5.Review.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string path = @"C:\Users\sonya\source\repos\Tyuiu.KoevaSJ.Sprint5.Review\Tyuiu.KoevaSJ.Sprint5.Review.V24\bin\Debug\OutPutFileTask7V24.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KoevaSJ.Sprint5.Review.V24/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Tyuiu.KoevaSJ.Sprint5.Review.V24.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint5.Review.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнила: Коева С.Ю.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Rewiev                                                            *");
+            Console.WriteLine("* Задание #Rewiev                                                         *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнила: Коева Софья Юрьевна | АСОиУБ-23-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть набор символьных данных.Заменить все русские    *");
+            Console.WriteLine("* слова на слово 'слово'. Полученный результат сохранить в файл           *");
+            Console.WriteLine("* OutPutDataFileTask7V24.txt.                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V24.txt";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Находиться в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
